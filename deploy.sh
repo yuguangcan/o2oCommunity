49a0f9d2a897678241fbe62419dc492e068d86f8
yuguangcan <xcan.yu@gmail.com>
2014-05-26 00:52:38 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 #地址
 domain="o2o@115.28.208.154"
 
-scp -r static/*  ${domain}:/home/o2o/webroot/static
-scp -r template/* ${domain}:/home/o2o/template/
+scp -r dist/static/*  ${domain}:/home/o2o/webroot/static
+scp -r dist/template/* ${domain}:/home/o2o/template/
